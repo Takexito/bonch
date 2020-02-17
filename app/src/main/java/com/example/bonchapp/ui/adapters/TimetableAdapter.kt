f4cca2c1ec9f00a9c6efb2498f69dc78f1c91ceb
Sergey <serega.putkin@yandex.ru>
2020-02-17 18:46:08 +0300

    feat: add TimetableAdapter

---
+++ app/src/main/java/com/example/bonchapp/ui/adapters/TimetableAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.bonchapp.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bonchapp.R
+import com.example.bonchapp.ui.model.SubjectDTO
+
+class TimetableAdapter(val context: Context) : RecyclerView.Adapter<TimetablePostHolder>() {
+
+    var subject = ArrayList<SubjectDTO>()
+
+    fun setSubjects(subjectList: List<SubjectDTO>){
+        this.subject.clear()
+        this.subject.addAll(subjectList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimetablePostHolder {
+        return TimetablePostHolder(
+            LayoutInflater.from(context)
+                .inflate(R.layout.item_subject, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = subject.size
+
+    override fun onBindViewHolder(holder: TimetablePostHolder, position: Int) {
+        holder.bind(subject[position])
+    }
+
+}
+
+class TimetablePostHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val textTime = itemView.findViewById<TextView>(R.id.subject_time)
+    val textName = itemView.findViewById<TextView>(R.id.subject_name)
+    val textType = itemView.findViewById<TextView>(R.id.subject_type)
+    val textProfessor = itemView.findViewById<TextView>(R.id.subject_professor)
+    val textCabinet = itemView.findViewById<TextView>(R.id.subject_cabinet)
+
+    fun bind(subject: SubjectDTO) {
+        textTime.text = subject.time
+        textName.text = subject.name
+        textType.text = subject.type
+        textProfessor.text = subject.professor
+        textCabinet.text = subject.cabinet
+    }
+}
---
+++ app/src/main/java/com/example/bonchapp/ui/model/SubjectDTO.kt
@@ -0,0 +1,9 @@
+package com.example.bonchapp.ui.model
+
+public class SubjectDTO {
+    val time = ""
+    val name = ""
+    val type = ""
+    val professor = ""
+    val cabinet = ""
+}
