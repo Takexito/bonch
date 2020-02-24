11992d99feeb7e44c1cd84e08e3f64fe12424caf
Takexito <Takexito@gmail.com>
2020-02-25 00:29:39 +0300

    fix full event Ui

--- app/src/main/java/com/example/bonchapp/presenter/EventPresenter.kt
+++ app/src/main/java/com/example/bonchapp/presenter/EventPresenter.kt
@@ -1,11 +1,13 @@
 package com.example.bonchapp.presenter
 
+import androidx.fragment.app.add
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.bonchapp.coordinator.MainCoordinator
 import com.example.bonchapp.model.repository.TestRep
 import com.example.bonchapp.ui.event.EventFragment
+import com.example.bonchapp.ui.event.FullEventFragment
 
 class EventPresenter(val context: EventFragment) {
 
@@ -15,7 +17,8 @@ class EventPresenter(val context: EventFragment) {
     var testData: LiveData<ArrayList<String>> = _testData
 
     fun onItemClick(pos: Int) {
-        MainCoordinator.navigateToFullEvent(context, pos)
+        //MainCoordinator.navigateToFullEvent(context, pos)
+        context.activity!!.supportFragmentManager.beginTransaction().add(FullEventFragment(), null).commit()
     }
 
     fun setDataFromApi() {
